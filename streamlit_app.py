88672a7b4192c4f0824c7005f4679e108e6e3732
Ganesh Venepally <ganesh.venepally@gmail.com>
2023-10-08 13:32:01 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import yfinance as yf
-from datetime import datetime, timedelta
+from datetime import datetime
 import pandas as pd
 
-def get_total_return_for_multiple_stocks(stock_list, start_date):
-    end_date = (datetime.strptime(start_date, '%Y-%m-%d') + timedelta(days=5)).strftime('%Y-%m-%d')
+def get_day1_return_for_multiple_stocks(stock_list, start_date, end_date):
     results = []
 
     for stock_name in stock_list:
@@ -13,23 +12,25 @@ def get_total_return_for_multiple_stocks(stock_list, start_date):
             results.append([start_date, end_date, stock_name, "No data available"])
             continue
 
-        data['Returns'] = data['Close'].pct_change().fillna(0)
-        total_return = (data['Returns'] + 1).prod() - 1
-        results.append([start_date, end_date, stock_name, f"{total_return:.2%}"])
+        # Calculate percent change from open to close for the first day
+        first_day_data = data.iloc[0]
+        day1_return = (first_day_data['Close'] - first_day_data['Open']) / first_day_data['Open']
+        results.append([start_date, end_date, stock_name, f"{day1_return:.2%}"])
 
     return results
 
-st.title('Stock Returns Calculator')
-st.write('Calculate total returns for the next 5 days from the provided start date.')
+st.title('Stock Day 1 Returns Calculator')
+st.write('Calculate returns from open to close for the first day between the provided start and end dates.')
 
 # User input
 stock_names = st.text_input("Enter the list of stock names separated by commas (e.g. AAPL,MSFT,GOOGL):")
 start_date = st.date_input("Enter the start date:")
+end_date = st.date_input("Enter the end date:", start_date)  # Default to start_date
 
 if st.button('Calculate Returns'):
     stock_list = [stock.strip() for stock in stock_names.split(',')]
-    results = get_total_return_for_multiple_stocks(stock_list, start_date.strftime('%Y-%m-%d'))
+    results = get_day1_return_for_multiple_stocks(stock_list, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'))
     
     # Convert results to DataFrame for display in Streamlit
-    df = pd.DataFrame(results, columns=["Start Date", "End Date", "Stock Name", "Returns"])
+    df = pd.DataFrame(results, columns=["Start Date", "End Date", "Stock Name", "Day 1 Returns"])
     st.table(df)
