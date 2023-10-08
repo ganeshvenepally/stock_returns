f3c2bd56c10cfc2981d5fb4511dc4aa2e6e1e620
Ganesh Venepally <ganesh.venepally@gmail.com>
2023-10-08 13:33:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import yfinance as yf
-from datetime import datetime
 import pandas as pd
 
-def get_day1_return_for_multiple_stocks(stock_list, start_date, end_date):
+def get_total_return_for_multiple_stocks(stock_list, start_date, end_date):
     results = []
 
     for stock_name in stock_list:
@@ -12,15 +11,13 @@ def get_day1_return_for_multiple_stocks(stock_list, start_date, end_date):
             results.append([start_date, end_date, stock_name, "No data available"])
             continue
 
-        # Calculate percent change from open to close for the first day
-        first_day_data = data.iloc[0]
-        day1_return = (first_day_data['Close'] - first_day_data['Open']) / first_day_data['Open']
-        results.append([start_date, end_date, stock_name, f"{day1_return:.2%}"])
+        total_return = ((data['Close'].iloc[-1] - data['Open'].iloc[0]) / data['Open'].iloc[0]) * 100
+        results.append([start_date, end_date, stock_name, f"{total_return:.2f}%"])
 
     return results
 
-st.title('Stock Day 1 Returns Calculator')
-st.write('Calculate returns from open to close for the first day between the provided start and end dates.')
+st.title('Stock Returns Calculator')
+st.write('Calculate total returns for a list of stocks between the provided start and end dates.')
 
 # User input
 stock_names = st.text_input("Enter the list of stock names separated by commas (e.g. AAPL,MSFT,GOOGL):")
@@ -29,8 +26,8 @@ end_date = st.date_input("Enter the end date:", start_date)  # Default to start_
 
 if st.button('Calculate Returns'):
     stock_list = [stock.strip() for stock in stock_names.split(',')]
-    results = get_day1_return_for_multiple_stocks(stock_list, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'))
+    results = get_total_return_for_multiple_stocks(stock_list, start_date.strftime('%Y-%m-%d'), end_date.strftime('%Y-%m-%d'))
     
     # Convert results to DataFrame for display in Streamlit
-    df = pd.DataFrame(results, columns=["Start Date", "End Date", "Stock Name", "Day 1 Returns"])
+    df = pd.DataFrame(results, columns=["Start Date", "End Date", "Stock Name", "Total Returns"])
     st.table(df)
